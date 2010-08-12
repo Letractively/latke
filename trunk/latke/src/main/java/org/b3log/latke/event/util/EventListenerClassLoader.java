e3aa6e374f775d16ea94de056363d5ae24585ad2
DL88250@gmail.com <DL88250@gmail.com@a1f9e945-d702-b364-d3a6-76a78a0a562b>
2010-08-12 13:50:26 +0000

    Added synchronized event management.

---
+++ trunk/latke/src/main/java/org/b3log/latke/event/util/EventListenerClassLoader.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2009, 2010, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.latke.event.util;
+
+import org.b3log.latke.servlet.AbstractServletListener;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.b3log.latke.event.AbstractEventListener;
+
+/**
+ * Loads the event listeners.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Aug 12, 2010
+ */
+public final class EventListenerClassLoader {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER =
+            Logger.getLogger(EventListenerClassLoader.class);
+
+    /**
+     * Private default constructor.
+     */
+    private EventListenerClassLoader() {
+    }
+
+    /**
+     * Loads event listener classes(extends from {@link AbstractEventListener})
+     * with the specified class directory path.
+     *
+     * @param classesDirPath the specified class directory path.
+     * @return a list of event listener classes
+     */
+    public static List<Class<AbstractEventListener<?>>> loadEventListenerClasses(
+            final String classesDirPath) {
+        final File eventListenerClassesDir = new File(classesDirPath);
+
+        final List<File> classFiles = getClassFiles(eventListenerClassesDir);
+        final List<Class<AbstractEventListener<?>>> ret =
+                loadClasses(classFiles);
+
+        return ret;
+    }
+
+    /**
+     * Gets class files with the specified class directory.
+     *
+     * @param classesDir the specified class directory
+     * @return a list of class files
+     */
+    private static List<File> getClassFiles(final File classesDir) {
+        final List<File> ret = new ArrayList<File>();
+        final File[] files = classesDir.listFiles(new FileFilter() {
+
+            @Override
+            public boolean accept(final File file) {
+//                if (file.isDirectory() || file.getName().endsWith(".class")) {
+                return true;
+//                } else {
+//                    return false;
+//                }
+            }
+        });
+
+        if (null != files) {
+            for (int i = 0; i < files.length; i++) {
+                final File file = files[i];
+                if (file.isFile()) {
+                    ret.add(file);
+                    LOGGER.trace("Got class file[" + file.getPath() + "]");
+                } else if (file.isDirectory()) {
+                    ret.addAll(getClassFiles(file));
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Loads classes with the specified class files.
+     *
+     * @param classFiles the specified class files
+     * @return a list of classes
+     */
+    private static List<Class<AbstractEventListener<?>>> loadClasses(
+            final List<File> classFiles) {
+        final List<Class<AbstractEventListener<?>>> ret =
+                new ArrayList<Class<AbstractEventListener<?>>>();
+        final ClassLoader classLoader =
+                AbstractEventListener.class.getClassLoader();
+
+        try {
+            for (final File classFile : classFiles) {
+                final String fullPath = classFile.getPath();
+                final String packageAndClassPath =
+                        AbstractServletListener.getClientRemoteServicePackage()
+                        + fullPath.substring(
+                        fullPath.lastIndexOf(File.separator));
+                String className = packageAndClassPath.replaceAll(
+                        File.separator.equals("\\") ? "\\\\" : // for windows
+                        File.separator, // others OS
+                        ".");
+                className = className.replace("/", ".");
+                // removes the .class extension of filename
+                className = className.substring(0, className.lastIndexOf("."));
+
+                LOGGER.trace("Loading class[name=" + className + "]");
+
+                @SuppressWarnings("unchecked")
+                final Class<AbstractEventListener<?>> clazz =
+                        (Class<AbstractEventListener<?>>) classLoader.loadClass(
+                        className);
+
+                ret.add(clazz);
+            }
+        } catch (final ClassNotFoundException e) {
+            LOGGER.fatal(e.getMessage(), e);
+        }
+
+        return ret;
+    }
+}
