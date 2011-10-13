fee9428e89c0844326b9de209206398ee85c8b3c
DL88250 <DL88250@gmail.com>
2011-10-13 01:36:38 +0000

    一只秒表，用于程序执行时间统计。

---
+++ latke/src/test/java/org/b3log/latke/util/StopwatchsTestCase.java
@@ -0,0 +1,73 @@
+
+/*
+ * Copyright (c) 2009, 2010, 2011, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.latke.util;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * {@link Stopwatchs} test case.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Oct 12, 2011
+ */
+public final class StopwatchsTestCase {
+
+    /**
+     * Tests method {@link Stopwatchs#getTimingStat()}.
+     * @throws Exception exception
+     */
+    @Test
+    public void getTimingStat() throws Exception {
+        Stopwatchs.start("task 1");
+
+        Stopwatchs.start("task 1.1");
+        final long task11Time = 50;
+        Thread.sleep(task11Time);
+        Stopwatchs.end(); // Ends 1.1
+
+
+        Stopwatchs.start("task 1.2");
+        Stopwatchs.start("task 1.2.1");
+        final long task121Time = 0;
+        Stopwatchs.end(); // Ends 1.2.1
+
+        Stopwatchs.start("task 1.2.2");
+        final long task122Time = 20;
+        Thread.sleep(task122Time);
+        Stopwatchs.end(); // Ends 1.2.2
+
+        Stopwatchs.end(); // Ends 1.2
+        final long task12Time = task121Time + task122Time;
+
+
+        Stopwatchs.start("task 1.3");
+        final long task13Time = 10;
+        Thread.sleep(task13Time);
+        Stopwatchs.end(); // Ends 1.3
+
+        Stopwatchs.end(); // Ends 1
+
+        System.out.println(Stopwatchs.getTimingStat());
+
+        Assert.assertEquals(Stopwatchs.getElapsed("task 1.1"), task11Time);
+        Assert.assertEquals(Stopwatchs.getElapsed("task 1.2"), task12Time);
+        Assert.assertEquals(Stopwatchs.getElapsed("task 1.2.1"), task121Time);
+        Assert.assertEquals(Stopwatchs.getElapsed("task 1.2.2"), task122Time);
+        Assert.assertEquals(Stopwatchs.getElapsed("task 1.3"), task13Time);
+    }
+}
