018cc0309902800956c680b2289e0af801d94b8d
DL88250 <DL88250@gmail.com>
2012-05-30 03:21:10 +0000

    修复秒表打印缩进问题。

--- latke/src/test/java/org/b3log/latke/util/StopwatchsTestCase.java
+++ latke/src/test/java/org/b3log/latke/util/StopwatchsTestCase.java
@@ -13,22 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Copyright (c) 2009, 2010, 2011, B3log Team
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.b3log.latke.util;
 
 import org.testng.annotations.Test;
@@ -37,7 +21,7 @@ import org.testng.annotations.Test;
  * {@link Stopwatchs} test case.
  *
  * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
- * @version 1.0.0.1, Oct 13, 2011
+ * @version 1.0.0.2, May 30, 2012
  */
 public final class StopwatchsTestCase {
 
@@ -75,6 +59,14 @@ public final class StopwatchsTestCase {
 
         Stopwatchs.start("task 1.2");
         Stopwatchs.start("task 1.2.1");
+        Stopwatchs.start("task 1.2.1.1");
+        Stopwatchs.start("task 1.2.1.1.1");
+        Stopwatchs.start("task 1.2.1.1.1.1");
+        Stopwatchs.start("task 1.2.1.1.1.1.1");
+        Stopwatchs.end(); // Ends 1.2.1.1.1.1.1
+        Stopwatchs.end(); // Ends 1.2.1.1.1.1
+        Stopwatchs.end(); // Ends 1.2.1.1.1
+        Stopwatchs.end(); // Ends 1.2.1.1
         Stopwatchs.end(); // Ends 1.2.1
 
         Stopwatchs.start("task 1.2.2");
